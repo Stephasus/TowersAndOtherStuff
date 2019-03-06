fdd4aa8f6dfca5ce0fbc315091220ca7eeb19d10
Stephan <stephan.klusowski@gmail.com>
2019-03-06 23:23:27 +0100

    first commit "hi mom"

---
+++ inc/header.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/default.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/default.js"></script>
+</head>
+
+
+
